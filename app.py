c9d305e464c382ad3f2b2057d9596d02da19dd1f
LuisGomes18 <luiscgomes0@gmail.com>
2025-12-03 13:29:51 +0000

    🐛 fix: Correction of the list of modules in the main file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from functions.temporary_manipulation import create_temporary_file
+from functions.tmp_manager import create_temporary_file
 from functions.convert_urls import converter_regex
 from menus.principal_menu import principal_menu
 
